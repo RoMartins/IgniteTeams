fc45db7dba2eceda48df1cff66dd4c818487b025
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2023-11-09 23:07:10 -0300

    feat: component listEmpty

---
+++ src/components/ListEmpty/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+interface CardEmptyProps {
+  message: string
+}
+
+export function ListEmpty({message}: CardEmptyProps){
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-[#7C7C8A] font-regular_roboto text-sm">
+        {message}
+      </Text>
+    </View>
+  )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,26 @@
-import { GroupCard } from '@assets/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlights';
 import { useState } from 'react';
 import {FlatList, View } from 'react-native';
+import {ListEmpty } from '@components/ListEmpty';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
-  const [ groups, SetGroups] = useState(['Galera rocketseat', 'ignite'])
+  const [ groups, SetGroups] = useState([])
   return (
     <View className="flex-1 bg-background-black">
       <Header />
       <Highlight title='Turmas' subTitle='Jogue com sua turma'/>
+
       <FlatList 
       data={groups}
       keyExtractor={item => item}
       renderItem={({item}) => (
         <GroupCard title={item}/>
+      )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+      ListEmptyComponent={() => (
+        <ListEmpty message='Que tal cadastrar um nova turma?'/>  
       )}
       />
     </View>
