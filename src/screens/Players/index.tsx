5cc3435520f822e51b5ef78967fc527643d08e6e
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2023-11-30 21:09:21 -0300

    feat: filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,14 @@
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlights";
 import { InputComponent } from "@components/Input";
-import { View } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, View } from "react-native";
 
 export function Players() {
+  const [teamSelected, setTeamSelected] = useState('Time A')
+  const [players, setPlayers] = useState([])
   return (
     <View className=" flex-1 p-6 bg-background ">
       <Header showHeaderArrowButton />
@@ -14,6 +18,25 @@ export function Players() {
         <InputComponent placeholder="Nome da pessoa"/>
         <ButtonIcon type="primary" icon="add"/>
       </View>
+
+
+      <View  className='items-center flex-row justify-center mt-8 mb-3 w-full'>
+        <FlatList 
+        data={['Time A', 'Time B']}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Filter  
+          title={item} 
+          isActive={item === teamSelected}
+          onPress={() => setTeamSelected(item)}
+          />
+          )}
+        horizontal
+      />
+    <Text className="text-gray-200 font-bold_roboto text-sm">
+       {players.length}
+    </Text>
+          </View>
     </View>
   )
 }
